8a4216afa49bd66b581b6427812ba23c60ffb0fe
ChandraPrakash Kistaiahgari <chandraprakash.n.kistaiahgari@gsk.com>
2019-07-04 05:55:13 -0700

    pyspark api usage cheatsheet

---
+++ set_env.sh
@@ -0,0 +1,8 @@
+export SPARK_HOME="/home/cnk45874/tools/spark-2.4.3-bin-hadoop2.7"
+export PATH="$SPARK_HOME/bin:$PATH"
+export JAVA_HOME="/home/cnk45874/tools/jdk1.8.0_211"
+export PATH="$JAVA_HOME/bin:$PATH"
+   
+export PYSPARK_SUBMIT_ARGS="pyspark-shell"
+export PYSPARK_DRIVER_PYTHON=ipython
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook' pyspark
