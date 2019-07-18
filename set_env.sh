5955a6779d565a0e299ea4fa8829f3ab835f2ab0
chandra-prakash-reddy <41863702+chandra-prakash-reddy@users.noreply.github.com>
2019-07-18 15:27:49 +0530

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
-export SPARK_HOME="/home/cnk45874/tools/spark-2.4.3-bin-hadoop2.7"
+export SPARK_HOME="/home/tools/spark-2.4.3-bin-hadoop2.7"
 export PATH="$SPARK_HOME/bin:$PATH"
-export JAVA_HOME="/home/cnk45874/tools/jdk1.8.0_211"
+export JAVA_HOME="/home/tools/jdk1.8.0_211"
 export PATH="$JAVA_HOME/bin:$PATH"
    
 export PYSPARK_SUBMIT_ARGS="pyspark-shell"
