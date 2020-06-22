38285de2e7893a4e135ea2971c9d25f4da7fc85f
chandraprakash kistaiahgari <reddyp148@gmail.com>
2020-06-22 23:32:26 +0530

    Update replace_file_word.py

--- replace_file_word.py
+++ replace_file_word.py
@@ -1,15 +1,6 @@
 import fnmatch
 import os
-import sys
-
-token_path=str(sys.argv[1])
-directory_path=str(sys.argv[2])
-print('token_file : {} directory_path : {}'.format(token_path,directory_path))
-with open(token_path, 'r') as token_file:
-    token = token_file.read()
-
-token = token.strip()
-token = token[2:token.__len__() - 2]
+import argparse
 
 
 def find_replace(directory, find, replace, filePattern):
@@ -23,4 +14,19 @@ def find_replace(directory, find, replace, filePattern):
                 f.write(s)
 
 
-find_replace(directory_path, "#vault_token#", token, "*.json")
+def update_token_in_settings(token_path,directory_path):
+    print('token_file : {} directory_path : {}'.format(token_path, directory_path))
+    with open(token_path, 'r') as token_file:
+        token = token_file.read()
+
+    token = token.strip()
+    token = token[2:token.__len__() - 2]
+
+    find_replace(directory_path, "#vault_token#", token, "*.json")
+
+
+parser = argparse.ArgumentParser(description='replace vault token')
+parser.add_argument('--t', help='provide token_file_name')
+parser.add_argument('--d', help='directory_path of settings json')
+args = parser.parse_args()
+update_token_in_settings(str(args.t),str(args.d))
