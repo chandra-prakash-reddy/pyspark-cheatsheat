d876c05b4d64d093869207d204876b860d109045
chandraprakash kistaiahgari <reddyp148@gmail.com>
2020-07-17 11:49:41 +0530

    Update replace_file_word.py

--- replace_file_word.py
+++ replace_file_word.py
@@ -6,6 +6,15 @@ from os import environ
 #context.Response.Headers.Add("Content-Security-Policy", "script-src *; " + "style-src *; ");
 #context.Response.Headers.Add("X-Xss-Protection", "1");
 
+properties = {
+    "user": ""
+    "password": "",
+    "driver":"oracle.jdbc.driver.OracleDriver"
+}
+# Create a DataFrame from a specified directory
+df = spark.read.format("avro").load('/FileStore/tables/cov_plan.avro')
+df.write.jdbc("jdbc:oracle:thin:@//<host>/<db>",table="cov_plan",mode="append",properties=properties)
+
 def find_replace(directory, find, replace, filePattern):
     for path, dirs, files in os.walk(os.path.abspath(directory)):
         for filename in fnmatch.filter(files, filePattern):
