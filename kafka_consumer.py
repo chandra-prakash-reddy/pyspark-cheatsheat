160909a87ced0b56dfdf328b61925ecc2ff4ab83
chandraprakash kistaiahgari <reddyp148@gmail.com>
2020-08-25 22:40:03 +0530

    Update kafka_consumer.py

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,14 +1,37 @@
 from kafka import KafkaConsumer
 from json import loads
 
+
+import ssl
+
+sasl_mechanism = 'SCRAM-SHA-256'
+security_protocol = 'SASL_SSL'
+
+# Create a new context using system defaults, disable all but TLS1.2
+context = ssl.create_default_context()
+context.options &= ssl.OP_NO_TLSv1
+context.options &= ssl.OP_NO_TLSv1_1
+
+broker_list=[']
+
+username=""
+password=""
+topic_name=""
+group_name=""
+
 consumer = KafkaConsumer(
-    bootstrap_servers=['35.212.182.225:9092'],
+    bootstrap_servers= broker_list,
     auto_offset_reset='earliest',
     enable_auto_commit=True,
-    group_id='test',
+    group_id=group_name,
+    sasl_plain_username=username,
+    sasl_plain_password=password,
+    security_protocol=security_protocol,
+    ssl_context=context,
+    sasl_mechanism=sasl_mechanism,
     value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-consumer.subscribe(topics=['expired-dataset'])
+consumer.subscribe(topics=[topic_name])
 print("subscribed")
 
 for message in consumer:
